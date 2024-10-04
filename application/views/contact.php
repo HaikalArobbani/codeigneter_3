f749ba1994e5bed1cb2d8f69ad88bb0db30649ae
HaikalArobbani <haik20208si@student.nurulfikri.ac.id>
2024-10-04 14:46:32 +0700

    commit 1

---
+++ application/views/contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view('_partials/head.php'); ?>
+</head>
+
+<body>
+	<?php $this->load->view('_partials/navbar.php'); ?>
+
+	<h1>Contact</h1>
+	<p>Ini halaman contact</p>
+	<form action="" method="post">
+		<div>
+			<label for="name">Name</label>
+			<input type="text" name="name" id="name" placeholder="Your Name">
+		</div>
+		<div>
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email" placeholder="Your Email"></label>
+		</div>
+		<div>
+			<label for="message">Message</label>
+			<input type="textarea" name="message" id="message" col="30" row="10" placeholder="Your Message">
+		</div>
+		<div>
+			<input type="button" class="btn btn-primary" value="Kirim">
+			<input type="reset" value="Reset">
+		</div>
+	</form>
+
+	<?php $this->load->view('_partials/footer.php'); ?>
+
+</body>
+
+</html>
